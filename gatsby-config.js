575008ed483825108fb250773704d3e36659aff0
Luis Manzanero <manzanero.luis1995@gmail.com>
2020-09-26 01:40:39 +0800

    fixed deployment bugs

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,11 +4,11 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: "Strapi Gatsby Blog/Shopping Cart",
+    title: "Strapi Gatsby Blog Shopping Cart",
     titleTemplate: "%s · Unleash content",
     description:
       "Strapi Gatsby Blog",
-    url: process.NODE_ENV.API_URL || "https://lmanzanero.herokuapp.com", // No trailing slash allowed!
+    url: process.env.API_URL || "http://localhost:1337", // No trailing slash allowed!
     image: "/uploads/default-image.jpeg", // Path to your image you placed in the 'static' folder
     twitterUsername: "@you",
   },
@@ -25,7 +25,7 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: process.NODE_ENV.API_URL || "https://lmanzanero.herokuapp.com",
+        apiURL: process.env.API_URL || "http://localhost:1337",
         contentTypes: [
           "article",
           "category",
